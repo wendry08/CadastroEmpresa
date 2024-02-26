e04f59fdd0c885a08e8959f446436f2c93e71d0c
wendry08 <wendry.fumaca@gmail.com>
2024-02-26 15:37:53 -0400

    segundo commit

--- cadastro de empresa - PWS/cadastro de empresa - PWS/Cadastro.cs
+++ cadastro de empresa - PWS/cadastro de empresa - PWS/Cadastro.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace cadastro_de_empresa___PWS
+﻿public class cadastro
 {
-    internal class Cadastro
+    public string NomeFantasia { get; set; }
+    public string Cnpj { get; set; }
+
+    public string Razao { get; set; }
+
+    public string Regime { get; set; }
+    public string Situacao { get; set; }
+
+    public string DataAtividade { get; set; }
+
+    public string Telefone { get; set; }
+
+    public string Capital { get; set; }
+
+    public string Estado { get; set; }
+
+    public string Cidade { get; set; }
+
+    public string Bairro { get; set; }
+
+    public string Rua { get; set; }
+
+    public string Tipo { get; set; }
+
+    public string Porte { get; set; }
+
+    public string Natureza { get; set; }
+
+    public string NomeProprietario { get; set; }
+
+    public string Cpf { get; set; }
+
+    public cadastro(string NomeFantasia, string Cnpj, string Razao, string Regime, string Situacao, string DataAtividade,
+        string Telefone, string Capital, string Estado, string Cidade, string Bairro, string Rua, string Tipo, string Porte,
+        string Natureza, string NomeProprietario, string Cpf)
     {
+        this.NomeFantasia = NomeFantasia;
+        this.Cnpj = Cnpj;
+        this.Razao = Razao;
+        this.Regime = Regime;
+        this.Situacao = Situacao;
+        this.DataAtividade = DataAtividade;
+        this.Telefone = Telefone;
+        this.Capital = Capital;
+        this.Estado = Estado;
+        this.Cidade = Cidade;
+        this.Bairro = Bairro;
+        this.Rua = Rua;
+        this.Tipo = Tipo;
+        this.Porte = Porte;
+        this.Natureza = Natureza;
+        this.NomeProprietario = NomeProprietario;
+        this.Cpf = Cpf;
+
+
+    }
+
+    public cadastro()
+    {
+
     }
 }
--- cadastro de empresa - PWS/cadastro de empresa - PWS/Form1.cs
+++ cadastro de empresa - PWS/cadastro de empresa - PWS/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -49,31 +50,36 @@ namespace cadastro_de_empresa___PWS
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string tipo2;
-            string cpnj = tx_cnpj.Text;
-            string razao = tx_razaosocial.Text;
-            string nomefantasia = tx_nomefantasia.Text;
-            string situacao = tx_situacaocadastral.Text;
-            string regime;
-            string dataatividade = tx_atividade.Text;
-            string telefone = tx_telefone.Text;
-            string estado = tx_completo.Text;
-            string bairro = tx_bairro.Text;
-            string rua = tx_rua.Text;
-            string empresa;
-            string natureza = tx_juridica.Text;
-            string nomepropriedade = tx_proprietario.Text;
-            string cpf = tx_cpf.Text;
 
-            if(tx_nacional.Checked == true)
+
+            cadastro cad = new cadastro();
+
+            string tipo2;
+            string regime;
+            string empresa;
+
+            cad.Cnpj = tx_cnpj.Text;
+            cad.Razao = tx_razaosocial.Text;
+            cad.NomeFantasia = tx_nomefantasia.Text;
+            cad.Situacao = tx_situacaocadastral.Text;
+            cad.DataAtividade = tx_atividade.Text;
+            cad.Telefone = tx_telefone.Text;
+            cad.Estado = tx_completo.Text;
+            cad.Bairro = tx_bairro.Text;
+            cad.Rua = tx_rua.Text;
+            cad.Natureza = tx_juridica.Text;
+            cad.NomeProprietario = tx_proprietario.Text;
+            cad.Cpf = tx_cpf.Text;
+
+            if (tx_nacional.Checked == true)
             {
-               regime = tx_nacional.Text;
+                regime = tx_nacional.Text;
             }
             else if (tx_presumido.Checked == true)
             {
                 regime = tx_presumido.Text;
             }
-            else 
+            else
             {
                 regime = tx_nomereal.Text;
             }
@@ -82,13 +88,13 @@ namespace cadastro_de_empresa___PWS
             {
                 empresa = tx_grande.Text;
             }
-            else if (tx_pequeno.Checked == true)
+            else if (tx_grande.Checked == true)
             {
                 empresa = tx_grande.Text;
             }
-            else 
+            else
             {
-                 empresa = tx_medio.Text;
+                empresa = tx_medio.Text;
             }
 
             if (tx_matriz.Checked == true)
@@ -99,11 +105,20 @@ namespace cadastro_de_empresa___PWS
             {
                 tipo2 = tx_filial.Text;
             }
-            MessageBox.Show("Cadastro realizado com sucesso!!!");
-            MessageBox.Show($"cnpj:{CNPJ} \n razao social é:{razao} \n nome fantasia:{nomefantasia}\n situacao cadastral:{situacao}\n +" +
-                $" regime tributario:{regime}\n data atividade {dataatividade}\n telefone:{telefone}\n estado:{estado}\n bairro:{bairro} +" +
-                $" rua:{rua}\n tipo:{tipo2}\n empresa:{empresa}\n natureza:{natureza}\n nome proprietario{nomepropriedade}\n cpf{cpf}" +
-                $" ");
+
+            if (Validar())
+            {
+                MessageBox.Show("Cadastro realizado com sucesso!!!");
+                MessageBox.Show($"cnpj:{cad.Cnpj} \n razao social é:{cad.Razao} \n nome fantasia:{cad.NomeFantasia}\n situacao cadastral:{cad.Situacao}\n +" +
+                    $" regime tributario:{regime}\n data atividade {cad.DataAtividade}\n telefone:{cad.Telefone}\n estado:{cad.Estado}\n bairro:{cad.Bairro} +" +
+                    $" rua:{cad.Rua}\n tipo:{tipo2}\n empresa:{empresa}\n natureza:{cad.Natureza}\n nome proprietario{cad.NomeProprietario}\n cpf{cad.Cpf}" +
+                    $" ");
+            }
+            else
+            {
+                MessageBox.Show("Preencha todos os campos!");
+            }
+
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
@@ -124,6 +139,69 @@ namespace cadastro_de_empresa___PWS
         private void tx_cnpj_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        public bool Validar()
+        {
+            bool validacao;
+
+            if (tx_nomefantasia.Text == "")
+            {
+                validacao = false;
+            }
+            else if (tx_atividade.Text == "")
+            {
+                validacao = false;
+            }
+            else if (tx_situacaocadastral.Text == "")
+            {
+                validacao = false;
+            }
+            else if (tx_telefone.Text == "")
+            {
+                validacao = false;
+            }
+            else if (tx_social.Text == "")
+            {
+                validacao = false;
+            }
+            else if (tx_completo.Text == "")
+            {
+                validacao = false;
+            }
+
+            else if (tx_bairro.Text == "")
+            {
+                validacao = false;
+            }
+            else if (tx_rua.Text == "")
+            {
+                validacao = false;
+            }
+            else if (tx_juridica.Text == "")
+            {
+                validacao = false;
+            }
+            else if (tx_completo.Text == "")
+            {
+                validacao = false;
+            }
+
+            else if (tx_proprietario.Text == "")
+            {
+                validacao = false;
+            }
+            else if (tx_cpf.Text == "")
+            {
+                validacao = false;
+            }
+
+            else
+            {
+                validacao = true;
+            }
+
+            return validacao;
         }
     }
 }
